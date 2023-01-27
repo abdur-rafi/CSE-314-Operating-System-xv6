7d23f315473397704dae263f1e2cfeabc478740d
abdur-rafi <abdurrafi403@gmail.com>
2023-01-27 22:46:11 +0600

    zemaphore implemented

--- Offline4NecessaryCodes/zemaphore/zemaphore.c
+++ Offline4NecessaryCodes/zemaphore/zemaphore.c
@@ -7,14 +7,27 @@
 #include <wait.h>
 #include "zemaphore.h"
 
-void zem_init(zem_t *s, int value) {
-  
+void zem_init(zem_t *s, int value)
+{
+    pthread_mutex_init(&s->lock, NULL);
+    pthread_cond_init(&s->cond, NULL);
+    s->val = value;
 }
 
-void zem_down(zem_t *s) {
+void zem_down(zem_t *s)
+{
+    pthread_mutex_lock(&s->lock);
+    --s->val;
+    if(s->val < 0)
+        pthread_cond_wait(&s->cond, &s->lock);
+    pthread_mutex_unlock(&s->lock);
 
 }
 
-void zem_up(zem_t *s) {
-
+void zem_up(zem_t *s)
+{
+    pthread_mutex_lock(&s->lock);
+    s->val++;
+    pthread_cond_signal(&s->cond);
+    pthread_mutex_unlock(&s->lock);
 }
