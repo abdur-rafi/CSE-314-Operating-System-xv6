211234286945810500722cdbb5894344f7cf1203
abdur-rafi <abdurrafi403@gmail.com>
2023-01-27 23:02:13 +0600

    task C complete maybe

--- Offline4NecessaryCodes/zemaphore/test-toggle.c
+++ Offline4NecessaryCodes/zemaphore/test-toggle.c
@@ -6,18 +6,24 @@
 #include <signal.h>
 #include <wait.h>
 #include <pthread.h>
+#include "zemaphore.h"
 
 #define NUM_THREADS 3
 #define NUM_ITER 10
+
+zemaphore phores[NUM_THREADS];
 
 void *justprint(void *data)
 {
   int thread_id = *((int *)data);
 
-  for(int i=0; i < NUM_ITER; i++)
-    {
-      printf("This is thread %d\n", thread_id);
-    }
+  int prev = (thread_id - 1 + NUM_THREADS) % NUM_THREADS;
+  for (int i = 0; i < NUM_ITER; i++)
+  {
+    zem_down(phores + prev);
+    printf("This is thread %d\n", thread_id);
+    zem_up(phores + thread_id);
+  }
   return 0;
 }
 
@@ -25,19 +31,23 @@ int main(int argc, char *argv[])
 {
 
   pthread_t mythreads[NUM_THREADS];
-  int mythread_id[NUM_THREADS];
 
-  
-  for(int i =0; i < NUM_THREADS; i++)
-    {
-      mythread_id[i] = i;
-      pthread_create(&mythreads[i], NULL, justprint, (void *)&mythread_id[i]);
-    }
-  
-  for(int i =0; i < NUM_THREADS; i++)
-    {
-      pthread_join(mythreads[i], NULL);
-    }
-  
+  int mythread_id[NUM_THREADS];
+  for(int i = 0; i < NUM_THREADS; ++i){
+    zem_init(phores + i, 0);
+  }
+  zem_init(phores + NUM_THREADS - 1, 1);
+
+  for (int i = 0; i < NUM_THREADS; i++)
+  {
+    mythread_id[i] = i;
+    pthread_create(&mythreads[i], NULL, justprint, (void *)&mythread_id[i]);
+  }
+
+  for (int i = 0; i < NUM_THREADS; i++)
+  {
+    pthread_join(mythreads[i], NULL);
+  }
+
   return 0;
 }
