82587c59e0e04aa7e38152085ee7f62db73473b0
shuaibw <1805010@ugrad.cse.buet.ac.bd>
2023-02-06 08:18:18 +0600

    Complete santacv

--- Offline4NecessaryCodes/santa/santa-cv.c
+++ Offline4NecessaryCodes/santa/santa-cv.c
@@ -7,20 +7,20 @@
 #define ELVES 6
 #define ELVES_GROUP 3
 
-int deerCount = 0, elvesCount = 0;
+int deerCount = 0, elvesCount = 0, hitchCount = 0, helpCount = 0;
 
 pthread_mutex_t elf_tex;
 pthread_mutex_t mutex;
 pthread_cond_t santa_cv;
 pthread_cond_t reindeer_cv;
 pthread_cond_t elf_cv;
-pthread_cond_t hiched;
+pthread_cond_t hitched;
 pthread_cond_t occupiedDeer;
 pthread_cond_t gettingHelp;
 
 void prepareSleigh() {
     printf("Santa preparing sleigh for deers\n");
-    //sleep(1);
+    // sleep(1);
 }
 
 void getHitched(int id) {
@@ -43,25 +43,24 @@ void* santa(void* arg) {
             prepareSleigh();
             printf("Sleigh prepared.Waiting for reindeers.\n");
             pthread_cond_broadcast(&reindeer_cv);
-            pthread_mutex_unlock(&mutex);
-            sleep(1);
-            pthread_mutex_lock(&mutex);
+            while (hitchCount < DEERS)
+                pthread_cond_wait(&hitched, &mutex);
             printf("All hitched. Going to give presents\n");
             deerCount = 0;
+            hitchCount = 0;
             printf("Santa returned\n");
             pthread_cond_broadcast(&occupiedDeer);
-            // pthread_mutex_lock(&mutex);
-        } 
+        }
         if (elvesCount == ELVES_GROUP) {
             printf("Santa is helping elves\n");
-            sleep(1);
-            // pthread_mutex_unlock(&mutex);
             pthread_cond_broadcast(&elf_cv);
+            while (helpCount < ELVES_GROUP)
+                pthread_cond_wait(&gettingHelp, &mutex);
+            helpCount = 0;
         }
-        // pthread_mutex_lock(&mutex);
-        printf("Santa back to sleeping\n");
+        printf("Santa goind back to sleep\n");
+        sleep(1);
         pthread_mutex_unlock(&mutex);
-
     }
 }
 
@@ -76,11 +75,14 @@ void* reeinder(void* arg) {
         }
         pthread_cond_wait(&reindeer_cv, &mutex);
         getHitched(id);
-
+        hitchCount++;
+        if (hitchCount == DEERS) {
+            pthread_cond_signal(&hitched);
+        }
         pthread_cond_wait(&occupiedDeer, &mutex);
         printf("%d reindeer going back\n", id);
         pthread_mutex_unlock(&mutex);
-        //sleep(1);
+        sleep(1 + rand() % 5);
     }
 }
 
@@ -95,18 +97,18 @@ void* elf(void* arg) {
             pthread_cond_signal(&santa_cv);
         else
             pthread_mutex_unlock(&elf_tex);
-        pthread_cond_wait(&elf_cv, &mutex);
-        pthread_mutex_unlock(&mutex);
 
+        pthread_cond_wait(&elf_cv, &mutex);
         getHelp(id);
-        //sleep(1);
-        pthread_mutex_lock(&mutex);
         elvesCount--;
+        helpCount++;
         if (elvesCount == 0) {
             printf("Elves group cleared\n");
             pthread_mutex_unlock(&elf_tex);
+            pthread_cond_signal(&gettingHelp);
         }
         pthread_mutex_unlock(&mutex);
+        sleep(1 + rand() % 5);
     }
 }
 
@@ -126,7 +128,7 @@ int main() {
     pthread_cond_init(&santa_cv, NULL);
     pthread_cond_init(&reindeer_cv, NULL);
     pthread_cond_init(&elf_cv, NULL);
-    pthread_cond_init(&hiched, NULL);
+    pthread_cond_init(&hitched, NULL);
     pthread_cond_init(&occupiedDeer, NULL);
     pthread_cond_init(&gettingHelp, NULL);
 
